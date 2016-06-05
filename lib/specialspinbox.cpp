bcb5a147275781cd6d307cbd953dc76e672d2bb8
french-paragon <french.paragon@gmail.com>
2016-06-05 13:47:12 +0200

    Store the last expression to reshow it if an error occured.

--- lib/specialspinbox.cpp
+++ lib/specialspinbox.cpp
@@ -6,16 +6,52 @@
 
 #include "simplemathexprparser.h"
 
-SpecialSpinBox::SpecialSpinBox(QWidget *parent) : QDoubleSpinBox(parent)
+SpecialSpinBox::SpecialSpinBox(QWidget *parent) :
+	QDoubleSpinBox(parent),
+	_lastExprParsed("0.0"),
+	_isLastValid(true)
 {
+
+}
+SpecialSpinBox::~SpecialSpinBox(){
+
+	//needed to avoid a segfault during destruction.
+	_isLastValid = true;
 
 }
 
 double SpecialSpinBox::valueFromText(const QString & text) const{
 
-	return utils::parseSimpleMathExpr(text, QMap<QString, double>());
+	_lastExprParsed = text;
+
+	bool ok;
+
+	double ret = utils::parseSimpleMathExpr(text, QMap<QString, double>(), &ok);
+
+		if(!ok){
+			_isLastValid = false;
+			emit errorWhileParsing(text);
+			return ret;
+		} else {
+
+			_isLastValid = true;
+			emit errorWhileParsing(text);
+			return ret;
+		}
+
+	return ret;
 
 }
+QString SpecialSpinBox::textFromValue(double val) const{
+
+	if(!_isLastValid){
+		return _lastExprParsed;
+	}
+
+	return QString("%1").arg(val);
+
+}
+
 QValidator::State SpecialSpinBox::validate ( QString & input, int & pos ) const{
 
 	return QValidator::Acceptable;
--- lib/specialspinbox.h
+++ lib/specialspinbox.h
@@ -10,15 +10,25 @@ class SIMPLEEXPRPARSERSHARED_EXPORT SpecialSpinBox : public QDoubleSpinBox
 	Q_OBJECT
 public:
 	explicit SpecialSpinBox(QWidget *parent = 0);
+	~SpecialSpinBox();
 
 	virtual double valueFromText(const QString & text) const;
+	virtual QString textFromValue(double val) const;
 	virtual QValidator::State validate ( QString & input, int & pos ) const;
 
 signals:
 
+	//! \brief signal emmitted when the last parsed expression create an error.
+	void errorWhileParsing(QString expr) const;
+	//! \brief signal emmitted when the last parsed expression is valid.
+	void noMoreParsingError() const;
+
 public slots:
 
 protected:
+
+	mutable QString _lastExprParsed;
+	mutable bool _isLastValid;
 };
 
 #endif // SPECIALSPINBOX_H
